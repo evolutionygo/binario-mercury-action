ac8d0edbd0924dd880c0db6e44fe59e9fbf6ad8d
termitaklk <112192503+termitaklk@users.noreply.github.com>
2024-10-18 08:35:13 -0400

    test

---
+++ scripts/build_ygopro.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+docker build -t ygopro-builder .
+
+docker create --name temp-container ygopro-builder
+
+mkdir -p ./build
+
+docker cp temp-container:/build_output/ygopro ./build/ygopro
+
+docker rm temp-container
+
+echo "El binario ygopro se ha copiado a la carpeta ./build"
